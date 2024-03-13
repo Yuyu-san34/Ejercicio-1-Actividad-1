a19341c820c112a207982e56e49d209fcfd257a8
Yuyu-san34 <160429198+Yuyu-san34@users.noreply.github.com>
2024-03-13 18:58:04 -0500

    Update App.java

--- App.java
+++ App.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args){
-        int edjuan,edalber,edmama,edana;
-        Scanner entrada= new Scanner(System.in);
-        edjuan= entrada.nextInt();
-        edalber=2*(edjuan/(3));
-        edana=4*(edjuan/3);
-        edmama=edjuan+edalber+edana;
-        System.out.println("LAS EDADES SON: ALBERTO="+edalber+"JUAN="+edjuan+"ANA="+edana+"MAMA="+edmama);
-        entrada.close();
-        }
-    }
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese código del empleado: ");
+        int codigoEmpleado = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Ingrese nombres del empleado: ");
+        String nombresEmpleado = scanner.nextLine();
+        System.out.print("Ingrese número de horas trabajadas al mes: ");
+        int horasTrabajadas = scanner.nextInt();
+        System.out.print("Ingrese valor de la hora trabajada: ");
+        double valorHora = scanner.nextDouble();
+        System.out.print("Ingres porcentaje de retención en la fuente (%): ");
+        double porcentajeRetencion = scanner.nextDouble();
+        double salarioBruto = horasTrabajadas * valorHora;
+        double retencion = salarioBruto * (porcentajeRetencion / 100);
+        double salarioNeto = salarioBruto - retencion;
+        System.out.println("\nInformación del empleado:");
+        System.out.println("Código del empleado: " + codigoEmpleado);
+        System.out.println("Nombres del empleado: " + nombresEmpleado);
+        System.out.println("Salario bruto: $" + salarioBruto);
+        System.out.println("Salario neto: $" + salarioNeto);
+        scanner.close();
 
 
